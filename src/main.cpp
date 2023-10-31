51e3a8fbc5a77c7ad7769544b0485d771485f78d
alexis-leclerc-uni <leca1404@usherbrooke.ca>
2023-10-31 13:54:27 -0400

    initiation du menu

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,16 @@ int aState;
 int aLastState;
 int totalColumns = 16;
 int totalRows = 2;
-int counter = 1000000;
+int counter = 1000001;
  
 LiquidCrystal_I2C lcd(0x27, totalColumns, totalRows);
+
+char menuItems[4][10] = {"Corona", "Biere", "Labatt", "Chepo"};
  
+void menu(int counter);
+void initMenu(char items[4][10]);
+
+
 void setup(){
   lcd.init();
   lcd.backlight(); // use to turn on and turn off LCD back light
@@ -22,6 +28,9 @@ void setup(){
   Serial.begin (9600);
   aLastState = digitalRead(outputA);  
 
+  initMenu(menuItems);
+
+/*
   lcd.setCursor(1,0);
   lcd.print("Corona");
   lcd.setCursor(1,1);
@@ -30,6 +39,8 @@ void setup(){
   lcd.print("Labatt");
   lcd.setCursor(10,1);
   lcd.print("Chepo");
+*/
+  menu(counter/2);
 }
  
 void loop() {
@@ -42,11 +53,27 @@ void loop() {
      } else {
        counter --;
      }
-     lcd.setCursor(0, counter/2%2);
-     Serial.println(counter/2%2);
-     lcd.print(">");
-     lcd.setCursor(0, (counter/2)%2-1);
-     lcd.print(" ");
    }
+   menu(counter/2);
    aLastState = aState; // Updates the previous state of the outputA with the current state
  }
+
+void menu(int counter){
+  lcd.setCursor(0, counter%2);
+  lcd.print(">");
+  lcd.setCursor(0, (counter)%2-1);
+  lcd.print(" ");
+}
+
+void initMenu(char items[4][10]){
+  //Serial.println(sizeof(items));
+  for(unsigned int i = 0; i <= sizeof(items) + 1; i++){
+    Serial.println(items[i]);
+    Serial.print("i : ");
+    Serial.println(i);
+    Serial.println((i%4)>2 ? 10 : 1);
+    Serial.println(i%2);
+    lcd.setCursor((i%4) >= 2 ? 10 : 1, (i % 2));
+    lcd.print(items[i]);
+  }
+}
